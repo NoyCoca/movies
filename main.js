19e9c7b4edf8debe2a4312d58741f19ecb2abc31
NoyCoca <noycoca123@gmail.com>
2021-04-11 22:31:51 +0300

    commit

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+class Movies {
+    
+}
+
+
 let baceApi = "https://api.tvmaze.com/search/shows?q="
 
 function printMovie(parsm) {
@@ -5,29 +10,64 @@ function printMovie(parsm) {
         .then(res => res.json())
 }
 let printM;
-async function movie() {
-    try {
+async function movie(param) {
+    try { 
+        main.innerHTML =""
         logImg.src = "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/b6e0b072897469.5bf6e79950d23.gif"
-        printM = await printMovie(inputId.value);
+        printM = await printMovie(param);
         if (printM.length == 0) {
-            noMatch.innerHTML = `<div>No match </div>`
-            printMovies.innerHTML = " "
+            main.innerHTML = `<h2> No match </h2>`
         }
         printM.forEach(movie => {
-            printMovies.innerHTML += `<div class ="movieCard"><h2>${movie.show.name}</h2>
+            main.innerHTML += `<div class ="movieCard"><h2>${movie.show.name}</h2>
             <p>${movie.show.genres} </p>
             <img src=${movie.show.image.medium}>
             <p> <a href=${movie.show.url} target="_blank">movie website</a></p></div>`
-
-            noMatch.innerHTML = ""
         });
     }
     catch (rej) {
-        noMatch.innerHTML = `<div>No match </div>`
-        printMovies.innerHTML = ""
+        let reject = [];
+        reject.push(rej)
     }
     finally {
         logImg.src = ""
+    }
+}
+
+function singUp() {
+    main.innerHTML =   `<div class="container">
+  <h2>sign up</h2>
+  <form>
+  <div class="form-group">
+      <label for="email">Uesr name:</label>
+      <input type="text" class="form-control" id="username" placeholder="User name">
+    </div>
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email" class="form-control" id="email" placeholder="Email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password confirm:</label>
+      <input type="password" class="form-control" id="pwdcon" placeholder="Enter password" name="pswd">
+      <p id="pwdchack"> </p>
+    </div>
+    </form>
+    <button type="submit" class="btn btn-primary" onclick="infoSand()">SAND</button>
+  
+</div>`
+
+}
+function infoSand() {
+    if(pwd.value == pwdcon.value){
+        pwdchack.innerHTML =""
+        main.innerHTML = `<h2> You will receive information for confirm</h2>`
+    }
+    else{
+        pwdchack.innerHTML = `The passwords are not the same`
     }
 }
 
